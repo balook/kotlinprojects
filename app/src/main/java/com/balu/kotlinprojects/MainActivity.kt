617348b2095f16ece17a81be1635de42157ed3f5
balook <balamanohar@gmail.com>
2021-11-29 07:58:05 +0530

    please commit

--- app/src/main/java/com/balu/kotlinprojects/MainActivity.kt
+++ app/src/main/java/com/balu/kotlinprojects/MainActivity.kt
@@ -3,20 +3,21 @@ package com.balu.kotlinprojects
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fileText: String = applicationContext.assets.open("temp.txt").bufferedReader().use {
-            it.readText()
+        val buttonClick  = findViewById<Button>(R.id.mybutton)
+        val textView = findViewById<TextView>(R.id.textView2)
+        var timesClicked = 0
+        buttonClick.setOnClickListener{
+            timesClicked += 1
+            textView.text = timesClicked.toString()
         }
-
-        print(fileText)
-
-        var intent = Intent(this,secondActivity::class.java)
-        startActivity(intent)
     }
 }
 
